ef856379f2c08d4c13fa235fb32dec8f6cbd9428
karthik-logs <karthik.periasami@mtap.in>
2016-11-23 11:31:31 +0530

    integrate firebase

--- app/src/main/java/com/github/karthyks/socialgameworks/splash/SplashActivity.java
+++ app/src/main/java/com/github/karthyks/socialgameworks/splash/SplashActivity.java
@@ -1,7 +1,9 @@
-package com.github.karthyks.socialgameworks;
+package com.github.karthyks.socialgameworks.splash;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.github.karthyks.socialgameworks.R;
 
 public class SplashActivity extends AppCompatActivity {
 
